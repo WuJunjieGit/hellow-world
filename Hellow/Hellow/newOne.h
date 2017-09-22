7e6c9d8e6815fd6f4b68f907b32db7c4dff5029e
Apple <apple@cernet2.net>
2017-09-22 20:01:52 +0800

    ds

---
+++ Hellow/Hellow/newOne.h
@@ -0,0 +1,13 @@
+//
+//  newOne.h
+//  Hellow
+//
+//  Created by Apple on 2017/9/22.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface newOne : NSObject
+
+@end
---
+++ Hellow/Hellow/newOne.m
@@ -0,0 +1,13 @@
+//
+//  newOne.m
+//  Hellow
+//
+//  Created by Apple on 2017/9/22.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#import "newOne.h"
+
+@implementation newOne
+
+@end
